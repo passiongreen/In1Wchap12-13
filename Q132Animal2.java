ec7d43d575284f99a2fc8baa37f0e4b03e012311
passiongreen <67631747+passiongreen@users.noreply.github.com>
2020-09-23 17:47:49 +0900

    Create Q132Animal2.java

---
+++ Q132Animal2.java
@@ -0,0 +1,69 @@
+//Main.java
+class Main {
+  public static void main(String[] args) {
+
+    Animal[] a = new Animal[2];
+    a[0] = new Dog("뭉치","치와와"); 
+    a[1] = new Cat("마이클", 7);
+
+    for (int i = 0 ; i < a.length ; i++) {
+      System.out.println("a[" + i + "] = " + a[i]);
+    }
+
+    for (int i = 0 ; i < a.length ; i++) {
+      System.out.print("a[" + i + "] = ");
+      a[i].introduce();
+    }
+  }
+}
+
+//Animal.java
+abstract class Animal {
+  private String name;
+  public Animal(String name) {this.name = name;}
+  public abstract void bark();
+  public String getName() { return name;}
+  public abstract String toString();
+  public void introduce() {
+    System.out.print(toString()+"이다.");
+    bark();
+  }
+}
+
+//Cat.java
+class Cat extends Animal {
+
+  private int age;
+  public Cat(String name, int age) {
+    super(name);this.age = age;
+  }
+
+  public void bark() { System.out.println("냐옹!");}
+
+  public String toString() {
+    return age + "의" + getName();
+  }
+}
+
+//Dog.java
+class Dog extends Animal {
+
+  private String type;
+
+
+
+  public Dog(String name, String type) {
+    super(name);this.type = type;
+  }
+
+
+
+  public void bark() { System.out.println("멍멍!");}
+
+  public String toString() {
+    return type + "의" + getName();
+  }
+}
+
+
+
